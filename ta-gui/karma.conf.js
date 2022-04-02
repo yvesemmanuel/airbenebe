fb75792bc4c02f048239ea295b169155208c3dba
Yves Emmanuel <yefo@cin.ufpe.br>
2022-04-02 19:31:03 +0100

    delete testing project

--- ta-gui/karma.conf.js
+++ ta-gui/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-project'),
+      dir: require('path').join(__dirname, './coverage/ta-gui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
