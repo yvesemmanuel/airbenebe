c76d63a8c1f4b5768512dcab5bbbe26ff166a005
Yves Emmanuel <83613347+yvesemmanuel@users.noreply.github.com>
2022-04-11 17:20:19 -0300

    toolbar and modals

--- airbenebe/src/app/app.module.ts
+++ airbenebe/src/app/app.module.ts
@@ -4,13 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button'
+
+import {ModalModule} from './modal/modal.module';
+
+import {MatFormField} from '@angular/material/form-field'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    ModalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
