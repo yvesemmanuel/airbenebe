dd2ecd9c6cded57448ab43080efc567f7dcaa808
dlondres42 <duml@cin.ufpe.br>
2022-05-10 13:21:15 -0300

    corrigindo erros dos testes de login

--- airbenebe/cypress/integration/renting-feature/login-component/login.component.spec.js
+++ airbenebe/cypress/integration/renting-feature/login-component/login.component.spec.js
@@ -6,25 +6,34 @@ describe("Teste de GUI do componente de acomodação", () => {
 
     it("Login bem sucedido", () => {
         // se usa um email valido no banco de dados
-        cy.get('[data-cy=email-input]').type('FelipoUlb@gmail.com');
-        cy.get('[data-cy=senha-input]').type('222');
-        cy.get('[data-cy=submitBtn]').click();
+        cy.get('[input-cy=email]').type('FelipoUlb@gmail.com');
+        cy.get('[input-cy=password]').type('222');
+        cy.get('[button-cy=logar]').click();
         cy.url().should('eq', 'http://localhost:4200/listing');
     });
 
     it("Email não registrado", () => {
-        cy.get('[data-cy=email-input]').type('hahaha@gmail.com');
-        cy.get('[data-cy=senha-input]').type('123');
-        cy.get('[data-cy=submitBtn]').click();
+        cy.get('[input-cy=email]').type('hahaha@gmail.com');
+        cy.get('[input-cy=password]').type('123');
+        cy.get('[button-cy=logar]').click();
         cy.contains('Nome de usuário ou senha incorretos.');
     });
 
     it("Senha incorreta", () => {
-        cy.get('[data-cy=email-input]').type('FelipoUlb@gmail.com'); //email válido
-        cy.get('[data-cy=senha-input]').type('123'); // senha incorreta
-        cy.get('[data-cy=submitBtn]').click();
+        cy.get('[input-cy=email]').type('FelipoUlb@gmail.com'); //email válido
+        cy.get('[input-cy=password]').type('123'); // senha incorreta
+        cy.get('[button-cy=logar]').click();
         cy.contains('Nome de usuário ou senha incorretos.');
     });
-    
 
+    it("Clicando no botão de entrar sem dados", () => {
+        cy.get('[button-cy=logar]').click();
+        cy.contains('Por favor, insira seu e-mail!');
+        cy.contains('Por favor, insira sua senha!');
+    });  
+
+    it("Botão não possui conta está redirecionando corretamente para registrar", () => {
+        cy.get('[button-cy=notRegistered]').click();
+        cy.url().should('eq', 'http://localhost:4200/register');
+    }); 
 });
